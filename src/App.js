f65ece5d493037a3234cdae6f3216412ad9b6e07
Nainsi Pandey <nancypandey6870@gmail.com>
2023-10-02 19:44:27 +0530

    tic-tac-toe

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+import { TicTacToe } from './component/tic-tac-toe';
+
+function App() {
+  return (
+    <div className="App">
+          <TicTacToe/>
+    </div>
+  );
+}
+export default App;
